be45e59d677f7af140876a153fcc2deb9509caf8
Daniel Emmel <s8daemme@stud.uni-saarland.de>
2019-07-29 13:34:07 +0200

    temporary fix for all single core shenanigans
    always spawning at least two threads for executor

--- src/main/java/com/pseuco/np19/project/rocket/UnitThread.java
+++ src/main/java/com/pseuco/np19/project/rocket/UnitThread.java
@@ -33,7 +33,7 @@ public class UnitThread extends Thread {
 
 	public UnitThread(Unit unit) {
 		this.unit = unit;
-		this.executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+		this.executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() + 1);
 		this.lock = new ReentrantLock();
 		this.terminating = lock.newCondition();
 		this.metadata = new Metadata(this.unit, this.executor, lock, terminating);
